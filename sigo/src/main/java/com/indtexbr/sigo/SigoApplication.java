cccc4706195cab58181c9c43dc6ea2395f0ec687
Nathalia Amaral <nathalia.mbrtech@outlook.com>
2020-12-10 21:22:22 -0300

    Initial commit

---
+++ sigo/src/main/java/com/indtexbr/sigo/SigoApplication.java
@@ -0,0 +1,13 @@
+package com.indtexbr.sigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigoApplication.class, args);
+	}
+
+}
---
+++ sigo/src/test/java/com/indtexbr/sigo/SigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indtexbr.sigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
